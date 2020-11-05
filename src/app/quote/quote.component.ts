d105d902775171dc7c5f421642bd2526f2c10738
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-05 22:56:57 +0200

    Added the date in put to the details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,14 +8,21 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
-    {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
-    {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
-    {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
-    {id:5,name:'Solve math homework',description:'Damn Math'},
-    {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
     ];
-
+  toggleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+    }
+  completeGoal(isComplete, index){
+      if (isComplete) {
+      this.quotes.splice(index,1);
+      }
+      }
   constructor() { }
 
   ngOnInit(): void {
